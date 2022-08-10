21365d5fb703df75530ce2dbdf70dc16d2215ab4
Rafael Ernesto Lopes <rflernesto@gmail.com>
2022-08-09 22:34:26 -0400

    refactor: change field value

--- src/main/java/dev/com/GreetingResource.java
+++ src/main/java/dev/com/GreetingResource.java
@@ -31,7 +31,7 @@ public class GreetingResource {
 
         Item item = new Item()
                 .withPrimaryKey("id", "3")
-                .withString("cpf", "04928416163")
+                .withString("cpf", "00000000000")
                 .withString("orderDate", "2022-08-11");
         return table.putItem(item);
     }
@@ -60,7 +60,7 @@ public class GreetingResource {
         QuerySpec spec = new QuerySpec()
                 .withKeyConditionExpression("cpf = :v_cpf")
                 .withValueMap(new ValueMap()
-                        .withString(":v_cpf","04928416163"));
+                        .withString(":v_cpf","00000000000"));
 
 
         ItemCollection<QueryOutcome> items = index.query(spec);
